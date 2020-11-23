341cd47844899b7b4483848df759bac2ed52e046
Rohan Perera <rohanpa2000@yahoo.com>
2020-11-23 19:26:39 +0530

    Update sp_all.sql

--- sql_scripts/sp_all.sql
+++ sql_scripts/sp_all.sql
@@ -141,3 +141,49 @@ BEGIN
 ;
 END$$
 DELIMITER ;
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS sp_select_last_incidents_image$$
+
+CREATE PROCEDURE sp_select_last_incidents_image 
+(
+)
+BEGIN
+(select 
+  			region_coordinates,
+			number_of_changes,
+			timestamp,
+			microseconds,
+			token,
+			path_to_image,
+			instance_name,
+            item_id,
+            tenant_id,
+            create_date
+            
+  from motion 
+  where instance_name = 'Court1'
+  order by create_date desc
+  LIMIT 20
+)
+  UNION ALL
+  (select 
+  			region_coordinates,
+			number_of_changes,
+			timestamp,
+			microseconds,
+			token,
+			path_to_image,
+			instance_name,
+            item_id,
+            tenant_id,
+            create_date
+  from motion 
+  where instance_name = 'Court2'
+  order by create_date desc
+  LIMIT 20
+)
+;
+END
+$$
+DELIMITER ;
