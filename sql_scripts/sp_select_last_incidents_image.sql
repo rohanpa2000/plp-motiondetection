061a4f8b715d6323198a314fc42ed76e170b59dd
Rohan Perera <rohanpa2000@yahoo.com>
2020-11-23 19:25:43 +0530

    Update sp_select_last_incidents_image.sql

--- sql_scripts/sp_select_last_incidents_image.sql
+++ sql_scripts/sp_select_last_incidents_image.sql
@@ -6,14 +6,17 @@ CREATE PROCEDURE sp_select_last_incidents_image
 )
 BEGIN
 (select 
-  			region_coordinates,
-			number_of_changes,
-			timestamp,
-			microseconds,
-			token,
-			path_to_image,
-			instance_name,
-            create_date
+  	region_coordinates,
+	number_of_changes,
+	timestamp,
+	microseconds,
+	token,
+	path_to_image,
+	instance_name,
+        item_id,
+        tenant_id,
+        create_date
+            
   from motion 
   where instance_name = 'Court1'
   order by create_date desc
@@ -21,14 +24,16 @@ BEGIN
 )
   UNION ALL
   (select 
-  			region_coordinates,
-			number_of_changes,
-			timestamp,
-			microseconds,
-			token,
-			path_to_image,
-			instance_name,
-            create_date
+  	region_coordinates,
+	number_of_changes,
+	timestamp,
+	microseconds,
+	token,
+	path_to_image,
+	instance_name,
+        item_id,
+        tenant_id,
+        create_date
   from motion 
   where instance_name = 'Court2'
   order by create_date desc
